7b039d81389d320b307d3996b729e3b425dd6c79
student <student@PC634S13.asost.local>
2015-06-25 12:49:07 +0900

    aaa

--- src/com/example/mekkemon/DBManager.java
+++ src/com/example/mekkemon/DBManager.java
@@ -23,16 +23,21 @@ public class DBManager extends SQLiteOpenHelper {
 	 * @param version DBバージョン（1~）
 	 */
 	public DBManager(Context context) {
-		super(context,"mekkemon.sqlite3",null,1);
+		super(context,"mekkemon.sqlite3",null,4);
 	}
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		db.execSQL("CPEAT TABLE IF NOT EXISTS"+
-		"mekkemon(shopid INTEGER PRIMARY KEY AUTOINCREMENT,shopname TEXT,photo BLOB,genreid INTEGER,price INTEGER,comment TEXT)");
-
+		db.execSQL("CREATE TABLE IF NOT EXISTS "+
+		"mise(shopid INTEGER PRIMARY KEY AUTOINCREMENT,shopname TEXT,photo BLOB,janruid INTEGER,price INTEGER,comment TEXT)");
 	}
+
+	public void onCreate1(SQLiteDatabase db) {
+		// TODO 自動生成されたメソッド・スタブ
+		db.execSQL("CREATE TABLE IF NOT EXISTS "+
+				"ichi(shopid INTEGER PRIMARY KEY AUTOINCREMENT,zx INTEGER,zy INTEGER)");
+	}
+
 
 	/* (非 Javadoc)
 	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
@@ -40,10 +45,16 @@ public class DBManager extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO 自動生成されたメソッド・スタブ
-		db.execSQL("DROP TABLE mekkemon");
+		db.execSQL("DROP TABLE mise");
 		onCreate(db);
-
 	}
+
+	public void onUpgrade1(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自動生成されたメソッド・スタブ
+		//db.execSQL("DROP TABLE ichi");
+		onCreate(db);
+	}
+
 
 
 }
--- src/com/example/mekkemon/Misetuika.java
+++ src/com/example/mekkemon/Misetuika.java
@@ -1,14 +1,29 @@
 package com.example.mekkemon;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class Misetuika extends Activity {
+
+	private SQLiteDatabase sqlDB;
+	DBManager dbm;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.misetuika);
+	}
+
+	protected void onResume() {
+		super.onResume();
+		dbm = new DBManager(this);
+		sqlDB = dbm.getWritableDatabase();
+	}
+
+	protected void onPause() {
+		super.onPause();
+		sqlDB.close();
 	}
 }
